49226fa2ad09fed4ba97653e0130e4edb222af39
Dharshak <132571038+DSnake333@users.noreply.github.com>
2025-03-01 21:09:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://beautiful-choux-191d4c.netlify.app", "http://localhost:8080"],
+    allow_origins=["https://football-match-analyzer.netlify.app", "http://localhost:8080"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
